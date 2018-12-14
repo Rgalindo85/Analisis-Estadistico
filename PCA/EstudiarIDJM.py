7a5e904880701b0a4e6efc8324e65cd4ad80a74f
Roger Galindo <rogerschrodinger@gmail.com>
2018-12-14 12:22:38 -0500

    Add files via upload
    Complete set of plots, catplot, barplot, boxplot and histograms for IDJM

--- PCA/EstudiarIDJM.py
+++ PCA/EstudiarIDJM.py
@@ -9,43 +9,31 @@ def EstudiarIDJM(data):
     # data_edad_22_25_mujer, data_edad_22_25_hombre = DividirDataPorSexo(data_edad_22_25)
     # data_edad_25_28_mujer, data_edad_25_28_hombre = DividirDataPorSexo(data_edad_25_28)
 
-    #fig, (ax1, ax2) = plt.subplots(1, 1, sharey=True, sharex=True)
-    #ax1 = data_edad_14_17_mujer['IDJM'].plot.box()
-    #ax1.set_title('De 14 a 17')
-    #ax2 = data_edad_14_17_hombre['IDJM'].plot.box()
-    #ax2.set_title('De 14 a 17: Hombre')
+    #---------------- Categorical Plots ---------------------------
+    CatPlot(data, col='Estrato', figsize=(10,6), ymin=0, ymax=80)
+    CatPlot(data, col='Comuna', figsize=(10,6), ymin=0, ymax=80)
 
-    # plt.boxplot([data_edad_14_17_mujer['IDJM'], data_edad_14_17_hombre['IDJM']],
-    #             labels=['Mujer', 'Hombre'])
-    # plt.show()
+    #---------------- Bar Plots ---------------------------
+    BarPlot(data, xname='Sexo',       yname='IDJM', hue = 'Rango Edad', ymin=0, ymax=80, figsize=(6,6) )
+    BarPlot(data, xname='Rango Edad', yname='IDJM', hue = 'Sexo',       ymin=0, ymax=80, figsize=(6,6) )
+    BarPlot(data, xname='Estrato',                     yname='IDJM', hue = 'Rango Edad', ymin=0, ymax=80, figsize=(10,6))
+    BarPlot(data, xname='Tiempo de Vivir en Medellin', yname='IDJM', hue = 'Rango Edad', ymin=0, ymax=80, figsize=(15,6))
+    BarPlot(data, xname='Comuna',                      yname='IDJM', hue = 'Rango Edad', ymin=0, ymax=80, figsize=(21,6))
 
+    #---------------- Box Plots ---------------------------
     BoxPlot(data, xname='Sexo',                        yname='IDJM', hue = 'Rango Edad', ymin=30, ymax=110)
     BoxPlot(data, xname='Rango Edad',                  yname='IDJM', hue = 'Sexo',       ymin=30, ymax=110)
     BoxPlot(data, xname='Estrato',                     yname='IDJM', hue = 'Rango Edad', ymin=30, ymax=110)
     BoxPlot(data, xname='Tiempo de Vivir en Medellin', yname='IDJM', hue = 'Rango Edad', ymin=30, ymax=110)
     BoxPlot(data, xname='Comuna',                      yname='IDJM', hue = 'Rango Edad', ymin=30, ymax=110)
 
+    #---------------- Histogram Plots ---------------------------
     Histogram(data, xname='IDJM', yname='# de Jovenes', nbins=25, grupo=None)
     Histogram(data, xname='IDJM', yname='# de Jovenes', nbins=25, grupo='Sexo')
     Histogram(data, xname='IDJM', yname='# de Jovenes', nbins=25, grupo='Rango Edad')
     Histogram(data, xname='IDJM', yname='# de Jovenes', nbins=25, grupo='Estrato')
     Histogram(data, xname='IDJM', yname='# de Jovenes', nbins=25, grupo='Comuna')
 
-
-
-    # ax_estrato = sns.boxplot(x='Estrato', y='IDJM', hue='Rango Edad', data=data)
-    # ax_estrato.set_ylim(30,110)
-    # plt.savefig('Figures/IDJM_estrato')
-    #
-    # ax_edad = sns.boxplot(x='Rango Edad', y='IDJM', hue='Sexo', data=data)
-    # ax_edad.set_ylim(30,110)
-    # plt.savefig('Figures/IDJM_edad')
-    #
-    #
-    #
-    # ax_tiempo = sns.boxplot(x='Tiempo de Vivir en Medellin', y='IDJM', hue='Rango Edad', data=data)
-    # ax_tiempo.set_ylim(30,110)
-    # plt.savefig('Figures/IDJM_tiempo')
 def Histogram(data, xname, yname, nbins, grupo):
     import seaborn as sns
     import matplotlib.pyplot as plt
@@ -54,41 +42,92 @@ def Histogram(data, xname, yname, nbins, grupo):
     import numpy as np
 
     title = ''
+    mean = 0.0; sigma = 0.0; y_max = 0.0
     if grupo == None:
-        title = 'IDJM: ' + 'Total'
+        title = 'IDJM ' + 'Total'
         mean   = data[xname].mean()
         median = data[xname].median()
         sigma  = data[xname].std()
+        y_max = data[xname].max()
 
-        print('mean:', mean)
-        print('median:', median)
-        print('sigma:', sigma)
+        # print('mean:', mean)
+        # print('median:', median)
+        # print('sigma:', sigma)
     else:
-        title = 'IDJM: ' + grupo
+        title = 'IDJM ' + grupo
+        #print( data[xname].groupby(data[grupo]).mean().shape )
 
     ax = data.hist(column=xname, bins=nbins, by=grupo, sharex=True, figsize=(15,10))
+    index = 0
+    for plot in ax.flatten():
+        plot.set_xlabel(xname)
+        plot.set_ylabel(yname)
+        plot.grid(axis='y', alpha=0.75)
+
+        if grupo == None:
+            plot.text(mean,    plot.get_ylim()[1], 'media = %0.1f' %mean)
+            plot.text(mean+15, plot.get_ylim()[1], 'std = %0.1f' %sigma)
+        else:
+            #print('grupo:', grupo, ' index:', index)
+            if grupo == 'Estrato':
+                mean  = data[xname].groupby(data[grupo]).mean()[index+1]
+                sigma = data[xname].groupby(data[grupo]).std()[index+1]
+            else:
+                #print('grupo:', grupo, ' index:', index)
+                if index == 21: continue
+
+                mean  = data[xname].groupby(data[grupo]).mean()[index]
+                sigma = data[xname].groupby(data[grupo]).std()[index]
+
+            plot.text(0,       plot.get_ylim()[1], 'media = %0.1f' %mean)
+            plot.text(mean+15, plot.get_ylim()[1], 'std = %0.1f' %sigma)
+            index += 1
 
 
     pl.suptitle(title)
     pl.xlabel(xname)
     pl.ylabel(yname)
+    pl.grid(axis='y', alpha=0.75)
 
-
-
-    #ax.set_xtitle(xname)
-    pl.show()
+    save_name = 'Figures/' + 'Histogram' + '_' + title
+    pl.savefig(save_name)
 
 def BoxPlot(data, xname, yname, hue, ymin, ymax):
     import seaborn as sns
     import matplotlib.pyplot as plt
 
-    plt.title('Indice de Desarrollo Juvenil de Medellin (2018)')
+    plt.title('Indice Promedio de Desarrollo Juvenil de Medellin (2018)')
     ax = sns.boxplot(x=xname, y=yname, hue=hue, data=data)
     ax.set_ylim(ymin,ymax)
     save_name = 'Figures/' + yname + '_' + xname
     plt.savefig(save_name)
     plt.clf()
 
+def BarPlot(data, xname, yname, hue, ymin, ymax, figsize):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    plt.figure(figsize=figsize)
+    plt.title('Indice Promedio de Desarrollo Juvenil de Medellin (2018)')
+    ax = sns.barplot(x=xname, y=yname, hue=hue, data=data, ci="sd", capsize=.1)
+    ax.set_ylim(ymin,ymax)
+
+    save_name = 'Figures/BarPlot' + yname + '_' + xname
+    plt.savefig(save_name)
+    plt.clf()
+
+def CatPlot(data, col, figsize, ymin, ymax):
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    plt.figure(figsize=figsize)
+    plt.title('Indice Promedio de Desarrollo Juvenil de Medellin (2018)')
+    ax = sns.catplot(x='Sexo', y = 'IDJM', hue='Rango Edad',
+                     col=col, data=data, kind='bar', col_wrap=3, ci='sd',
+                     capsize=.1, height=2.5, aspect=.8)
+    #ax.set_ylim(ymin,ymax)
+
+    plt.show()
 
 def DividirDataPorSexo(data):
     mujer  = GetData(data, col='Sexo', item='Mujer')
